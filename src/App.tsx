329b08e116fee4a0259abf2138a149db7614158e
aoki <aoki.lcs1@gmail.com>
2025-05-04 20:20:01 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+import { authRepository } from './modules/auth/auth.repository';
+import { useCurrentUserStore } from './modules/auth/current-user.state';
+import { Home } from './pages/Home';
+import NoteDetail from './pages/NoteDetail';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const currentUseStore = useCurrentUserStore();
+
+  useEffect(() => {
+    setSession();
+  }, []);
+
+  const setSession = async () => {
+    const currentUser = await authRepository.getCurrentUser();
+    currentUseStore.set(currentUser);
+    setIsLoading(false);
+  };
+
+  if (isLoading) return <div />;
+
+  return (
+    <BrowserRouter>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/notes/:id" element={<NoteDetail />} />
+          </Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
